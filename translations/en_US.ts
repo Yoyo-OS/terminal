bd09689c88ed4aa4154650e1dbce9a088b558fd3
柚子 <40852301+uiYzzi@users.noreply.github.com>
2023-01-27 14:46:56 +0800

    Update en_US.ts

--- translations/en_US.ts
+++ translations/en_US.ts
@@ -24,27 +24,138 @@
     <message>
         <location filename="../src/qml/SettingsDialog.qml" line="10"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Settings</translation>
+    </message>
+</context>
+<context>
+    <name>GlobalSettings</name>
+    <message>
+        <location filename="../src/qml/GlobalSettings.qml" line="31"/>
+        <source>Baidu</source>
+        <translation>Baidu</translation>
+    </message>
+</context>
+<context>
+    <name>Bookmark</name>
+    <message>
+        <location filename="../src/qml/SettingsPages/Bookmark.qml" line="7"/>
+        <location filename="../src/qml/SettingsPages/Bookmark.qml" line="17"/>
+        <source>Bookmark</source>
+        <translation>Bookmark</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsDialog.qml" line="39"/>
+        <location filename="../src/qml/SettingsPages/Bookmark.qml" line="50"/>
+        <source>Directory address</source>
+        <translation>Directory address</translation>
+    </message>
+</context>
+<context>
+    <name>Custom</name>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="7"/>
+        <source>Custom</source>
+        <translation>Custom</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="17"/>
+        <source>Terminal</source>
+        <translation>Terminal</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="25"/>
+        <source>Word Characters</source>
+        <translation>Word Characters</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="40"/>
+        <source>Search Engines</source>
+        <translation>Search Engines</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="82"/>
+        <source>Search engine name</source>
+        <translation>Search engine name</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="90"/>
+        <source>Search engine url,Replace the keyword with {keyword}</source>
+        <translation>Search engine url,Replace the keyword with {keyword}</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsSideBar</name>
+    <message>
+        <location filename="../src/qml/SettingsSideBar.qml" line="34"/>
+        <source>Appearance</source>
+        <translation>Appearance</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsSideBar.qml" line="41"/>
+        <source>Bookmark</source>
+        <translation>Bookmark</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsSideBar.qml" line="48"/>
+        <source>Custom</source>
+        <translation>Custom</translation>
+    </message>
+</context>
+<context>
+    <name>Appearance</name>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="7"/>
+        <source>Appearance</source>
+        <translation>Appearance</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="39"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>Font</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsDialog.qml" line="63"/>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="63"/>
         <source>Font Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Font Size</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsDialog.qml" line="82"/>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="71"/>
+        <source>Color Scheme</source>
+        <translation>Color Scheme</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="79"/>
+        <source>Light Color Scheme</source>
+        <translation>Light Color Scheme</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="95"/>
+        <source>Dark Color Scheme</source>
+        <translation>Dark Color Scheme</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="114"/>
+        <source>Keyboard Cursor</source>
+        <translation>Keyboard Cursor</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="123"/>
+        <source>Shape</source>
+        <translation>Shape</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="146"/>
+        <source>Window effects</source>
+        <translation>Window effects</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="82"/>
         <source>Transparency</source>
-        <translation type="unfinished"></translation>
+        <translation>Transparency</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsDialog.qml" line="101"/>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="101"/>
         <source>Window Blur</source>
-        <translation type="unfinished"></translation>
+        <translation>Window Blur</translation>
     </message>
 </context>
 <context>
@@ -52,38 +163,58 @@
     <message>
         <location filename="../src/qml/Terminal.qml" line="178"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Copy</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="184"/>
         <location filename="../src/qml/Terminal.qml" line="198"/>
         <source>Paste</source>
-        <translation type="unfinished"></translation>
+        <translation>Paste</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="228"/>
+        <source>Clear Screen</source>
+        <translation>Clear Screen</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="234"/>
+        <source>Bookmark</source>
+        <translation>Bookmark</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="261"/>
+        <source>Search</source>
+        <translation>Search</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="248"/>
+        <source>Add to bookmark</source>
+        <translation>Add to bookmark</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="204"/>
         <source>Select All</source>
-        <translation type="unfinished"></translation>
+        <translation>Select All</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="209"/>
         <source>Open File Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>Open File Manager</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="214"/>
         <source>Exit full screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit full screen</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="214"/>
         <source>Full screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Full screen</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="221"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Settings</translation>
     </message>
 </context>
 </TS>
