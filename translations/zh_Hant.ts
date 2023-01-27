31ee8b91f77c37b52b9f3e52d40b922c3905f49b
柚子 <40852301+uiYzzi@users.noreply.github.com>
2023-01-27 14:41:54 +0800

    Update zh_Hant.ts

--- translations/zh_Hant.ts
+++ translations/zh_Hant.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="49"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation>進程正在運行，您確定要退出嗎？</translation>
+        <translation>有进程在运行中，您确定要退出吗？</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="63"/>
         <source>OK</source>
-        <translation>確認</translation>
+        <translation>确定</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="57"/>
@@ -24,27 +24,138 @@
     <message>
         <location filename="../src/qml/SettingsDialog.qml" line="10"/>
         <source>Settings</source>
-        <translation>設定</translation>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>GlobalSettings</name>
+    <message>
+        <location filename="../src/qml/GlobalSettings.qml" line="31"/>
+        <source>Baidu</source>
+        <translation>百度</translation>
+    </message>
+</context>
+<context>
+    <name>Bookmark</name>
+    <message>
+        <location filename="../src/qml/SettingsPages/Bookmark.qml" line="7"/>
+        <location filename="../src/qml/SettingsPages/Bookmark.qml" line="17"/>
+        <source>Bookmark</source>
+        <translation>书签</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsDialog.qml" line="39"/>
+        <location filename="../src/qml/SettingsPages/Bookmark.qml" line="50"/>
+        <source>Directory address</source>
+        <translation>目录地址</translation>
+    </message>
+</context>
+<context>
+    <name>Custom</name>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="7"/>
+        <source>Custom</source>
+        <translation>自定义</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="17"/>
+        <source>Terminal</source>
+        <translation>终端</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="25"/>
+        <source>Word Characters</source>
+        <translation>双击可选中的特殊字符</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="40"/>
+        <source>Search Engines</source>
+        <translation>搜索引擎</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="82"/>
+        <source>Search engine name</source>
+        <translation>搜索引擎名称</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Custom.qml" line="90"/>
+        <source>Search engine url,Replace the keyword with {keyword}</source>
+        <translation>搜索引擎地址，用{keyword}代替关键词</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsSideBar</name>
+    <message>
+        <location filename="../src/qml/SettingsSideBar.qml" line="34"/>
+        <source>Appearance</source>
+        <translation>外观</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsSideBar.qml" line="41"/>
+        <source>Bookmark</source>
+        <translation>书签</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsSideBar.qml" line="48"/>
+        <source>Custom</source>
+        <translation>自定义</translation>
+    </message>
+</context>
+<context>
+    <name>Appearance</name>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="7"/>
+        <source>Appearance</source>
+        <translation>外观</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="39"/>
         <source>Font</source>
-        <translation>字型</translation>
+        <translation>字体</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsDialog.qml" line="63"/>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="63"/>
         <source>Font Size</source>
-        <translation>文字大小</translation>
+        <translation>字体大小</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsDialog.qml" line="82"/>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="71"/>
+        <source>Color Scheme</source>
+        <translation>配色</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="79"/>
+        <source>Light Color Scheme</source>
+        <translation>浅色模式</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="95"/>
+        <source>Dark Color Scheme</source>
+        <translation>深色模式</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="114"/>
+        <source>Keyboard Cursor</source>
+        <translation>光标</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="123"/>
+        <source>Shape</source>
+        <translation>样式</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="146"/>
+        <source>Window effects</source>
+        <translation>窗口效果</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="82"/>
         <source>Transparency</source>
         <translation>透明度</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsDialog.qml" line="101"/>
+        <location filename="../src/qml/SettingsPages/Appearance.qml.qml" line="101"/>
         <source>Window Blur</source>
-        <translation>視窗模糊度</translation>
+        <translation>窗口模糊</translation>
     </message>
 </context>
 <context>
@@ -52,38 +163,58 @@
     <message>
         <location filename="../src/qml/Terminal.qml" line="178"/>
         <source>Copy</source>
-        <translation>拷貝</translation>
+        <translation>复制</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="184"/>
         <location filename="../src/qml/Terminal.qml" line="198"/>
         <source>Paste</source>
-        <translation>貼上</translation>
+        <translation>粘贴</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="228"/>
+        <source>Clear Screen</source>
+        <translation>清空屏幕</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="234"/>
+        <source>Bookmark</source>
+        <translation>书签</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="261"/>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="248"/>
+        <source>Add to bookmark</source>
+        <translation>添加当前目录到书签</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="204"/>
         <source>Select All</source>
-        <translation>選擇全部</translation>
+        <translation>全选</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="209"/>
         <source>Open File Manager</source>
-        <translation>在檔案管理員中打開</translation>
+        <translation>打开文件管理器</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="214"/>
         <source>Exit full screen</source>
-        <translation>退出全螢幕</translation>
+        <translation>退出全屏</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="214"/>
         <source>Full screen</source>
-        <translation>全螢幕</translation>
+        <translation>全屏</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="221"/>
         <source>Settings</source>
-        <translation>設定</translation>
+        <translation>设置</translation>
     </message>
 </context>
 </TS>
